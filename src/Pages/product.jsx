629f2c721f90dc9e618b1666992fa00d9c275006
junz <farhan.juna@sigma.co.id>
2024-02-28 10:32:32 +0700

    Hide Total Price When == null

--- src/Pages/product.jsx
+++ src/Pages/product.jsx
@@ -1,6 +1,6 @@
 import CardProduct from "../components/Fragments/CardProducts"
 import Button from "../components/elements/Button"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 const products = [
     {
         id: 1,
@@ -21,7 +21,7 @@ const products = [
         title: "Macbook Pro M1",
         price: 40000,
         image: "images/laptop.jpg",
-        description: "Description for M1"        
+        description: "Description for M1"
     },
     {
         id: 4,
@@ -36,18 +36,19 @@ const ProductsPage = () => {
     const [cart, setCart] = useState([])
     const [totalPrice, setTotalPrice] = useState(0)
 
+    // Use Effect => Data Langsung berubah
     useEffect(() => {
         setCart(JSON.parse(localStorage.getItem("cart")) || [])
     }, []);
-    
-    useEffect(()=>{
-        if (cart.length > 0){
+
+    useEffect(() => {
+        if (cart.length > 0) {
             const sum = cart.reduce((acc, item) => {
-            const product = products.find((product) => product.id === item.id);
-            return acc + product.price * item.qty;
-        }, 0);
-        setTotalPrice(sum)  
-        localStorage.setItem(cart, JSON.stringify(cart))
+                const product = products.find((product) => product.id === item.id);
+                return acc + product.price * item.qty;
+            }, 0);
+            setTotalPrice(sum)
+            localStorage.setItem("cart", JSON.stringify(cart))
         }
     }, [cart])
 
@@ -59,62 +60,72 @@ const ProductsPage = () => {
     const email = localStorage.getItem('email')
 
     const handleAddToCart = (id) => {
-        if(cart.find(item => item.id === id)) {
+        if (cart.find(item => item.id === id)) {
             setCart(
-                cart.map(item => item.id === id ? {...item, qty: item.qty + 1} : item)
+                cart.map(item => item.id === id ? { ...item, qty: item.qty + 1 } : item)
             )
         } else {
-            setCart([...cart, {id, qty: 1}])
+            setCart([...cart, { id, qty: 1 }])
         }
     }
+// UseRef => Data disimpan dan tidak langsung berubah
+    const totalPriceRef = useRef(null)
+    useEffect(()=>{
+        if (cart.length > 0) {
+            totalPriceRef.current.style.display = "table-row";
+        } else {
+            totalPriceRef.current.style.display = "none";
+        }
+    }, [cart])
+
     return (
         <>
-        <div className="flex justify-end h-20 bg-blue-500 text-white px-10 items-center">
-            <p>{email}</p>
-            <Button color="ml-5 bg-black" onClick={handleLogout}>Logout</Button>
-        </div>
-        <div className="flex justify-center py-5">
-        <div className="w-3/4 flex flex-wrap">
-        {products.map((products)=>(
-                    <CardProduct key={products.id}>
-                    <CardProduct.Header image={products.image}/>
-                    <CardProduct.Body title={products.name}>{products.description}</CardProduct.Body>
-                    <CardProduct.Footer price={`${(products.price).toLocaleString("id-ID", {styles:"currency", currency:"IDR"})}`} handleAddToCart={handleAddToCart} id={products.id}/>
-                    </CardProduct>
-            ))}
-        </div>
-        <div className="w-1/4">
-            <h1 className="text-3xl font-bold text-blue-500">Cart</h1>
-            <table className="text-left table-auto border-separate  border-spacing-x-2">
-                <thead>
-                    <tr>
-                        <th>Product</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                { cart.map((item)=>{
-                        const product = products.find(
-                            (product) => product.id === item.id);
-                        return (
-                            <tr key={item.id}>
-                                <td>{product.title}</td>
-                                <td> {product.price.toLocaleString("id-ID", {styles:"currency", currency:"IDR"})}</td>
-                                <td>{item.qty}</td>
-                                <td>Rp. {(product.price * item.qty).toLocaleString("id-ID", {styles:"currency", currency:"IDR"})}</td>
+            <div className="flex justify-end h-20 bg-blue-500 text-white px-10 items-center">
+                <p>{email}</p>
+                <Button color="ml-5 bg-black" onClick={handleLogout}>Logout</Button>
+            </div>
+            <div className="flex justify-center py-5">
+                <div className="w-3/4 flex flex-wrap">
+                    {products.map((products) => (
+                        <CardProduct key={products.id}>
+                            <CardProduct.Header image={products.image} />
+                            <CardProduct.Body title={products.name}>{products.description}</CardProduct.Body>
+                            <CardProduct.Footer price={`${(products.price).toLocaleString("id-ID", { styles: "currency", currency: "IDR" })}`} handleAddToCart={handleAddToCart} id={products.id} />
+                        </CardProduct>
+                    ))}
+                </div>
+                <div className="w-1/4">
+                    <h1 className="text-3xl font-bold text-blue-500">Cart</h1>
+                    <table className="text-left table-auto border-separate  border-spacing-x-2">
+                        <thead>
+                            <tr>
+                                <th>Product</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Total</th>
                             </tr>
-                        )
-                    })}
-                    <tr className="font-bold">
-                        <td colSpan={3}>Total Price</td>
-                        <td>Rp. {(totalPrice).toLocaleString("id-ID", {styles:"currency", currency:"IDR"})}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        </div>
+                        </thead>
+                        <tbody>
+                            {cart.map((item) => {
+                                const product = products.find(
+                                    (product) => product.id === item.id);
+                                return (
+                                    <tr key={item.id}>
+                                        <td>{product.title}</td>
+                                        <td> {product.price.toLocaleString("id-ID", { styles: "currency", currency: "IDR" })}</td>
+                                        <td>{item.qty}</td>
+                                        <td>Rp. {(product.price * item.qty).toLocaleString("id-ID", { styles: "currency", currency: "IDR" })}</td>
+                                    </tr>
+                                )
+                            })}
+                            <tr className="font-bold" ref={totalPriceRef}>
+                                <td colSpan={3}>Total Price</td>
+                                <td>Rp. {(totalPrice).toLocaleString("id-ID", { styles: "currency", currency: "IDR" })}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </>
     )
 }
