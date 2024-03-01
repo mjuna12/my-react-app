c28a491b7b64981e5507034df2eab1d9aa1c42f9
junz <farhan.juna@sigma.co.id>
2024-03-01 10:21:55 +0700

    Adujst Store for Add Cart

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { data } from "autoprefixer";
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        data: JSON.parse(localStorage.getItem("cart")) || [],
+    },
+    reducers:{
+        addTocart: (state,action) => {
+            const itemInCart = state.data.find(item => item.id === action.payload.id);
+            if (itemInCart){
+                itemInCart.qty++
+            } else {
+            state.data.push(action.payload)
+            }
+        }
+    }
+})
+
+export const { addTocart } = cartSlice.actions;
+export default cartSlice.reducer;
