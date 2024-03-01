2160c49c8305878413b1fceb7095396773cdd84f
junz <farhan.juna@sigma.co.id>
2024-03-01 10:22:09 +0700

    Move to Header Global

---
+++ src/components/Fragments/TableCart.jsx
@@ -0,0 +1,61 @@
+import { useSelector } from "react-redux";
+import { useState, useEffect, useRef } from "react";
+
+const TableCart = (props) => {
+    const { products } = props;
+    const cart = useSelector((state) => state.cart.data);
+    const [totalPrice, setTotalPrice] = useState(0);
+    useEffect(() => {
+        if (products.length > 0 && cart.length > 0) {
+            const sum = cart.reduce((acc, item) => {
+                const product = products.find((product) => product.id === item.id);
+                return acc + product.price * item.qty;
+            }, 0);
+            setTotalPrice(sum)
+            localStorage.setItem("cart", JSON.stringify(cart))
+        }
+    }, [cart, products])
+
+    // UseRef => Data disimpan dan tidak langsung berubah
+    const totalPriceRef = useRef(null)
+    useEffect(()=>{
+        if (cart.length > 0) {
+            totalPriceRef.current.style.display = "table-row";
+        } else {
+            totalPriceRef.current.style.display = "none";
+        }
+    }, [cart])
+
+    return (
+        <table className="text-left table-auto border-separate  border-spacing-x-2">
+        <thead>
+            <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            {products.length > 0 && cart.map((item) => {
+                const product = products.find(
+                    (product) => product.id === item.id);
+                return (
+                    <tr key={item.id}>
+                        <td>{product.title}</td>
+                        <td> {product.price.toLocaleString("id-ID", { styles: "currency", currency: "USD" })}</td>
+                        <td>{item.qty}</td>
+                        <td>$ {(product.price * item.qty).toLocaleString("id-ID", { styles: "currency", currency: "USD" })}</td>
+                    </tr>
+                )
+            })}
+            <tr className="font-bold" ref={totalPriceRef}>
+                <td colSpan={3}>Total Price</td>
+                <td>$ {(totalPrice).toLocaleString("id-ID", { styles: "currency", currency: "USD" })}</td>
+            </tr>
+        </tbody>
+    </table>
+    )
+}
+
+export default TableCart;
